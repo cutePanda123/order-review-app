af2e785ddc639e0a2466916201a3d65fc87b587a
Peter Li <peterli1234@126.com>
2021-01-01 12:27:05 -0800

    moved test data to public folder and fetch the data by http request in the code

--- src/components/OrderList/index.js
+++ src/components/OrderList/index.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
 import OrderItem from '../OrderItem';
-import { orders } from '../../testData';
 
 class OrderList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            orders: []
+        };
+    }
+
+    componentDidMount() {
+        fetch('/mock/orders.json').then(res => {
+            if (res.ok) {
+                res.json().then(data => {
+                    this.setState({
+                        orders: data
+                    })
+                });
+            }
+        });
+    }
+
     render() {
         return (
-            <div>
-                {
-                    orders.map(order => {
-                        return (
-                            <OrderItem
-                                key={order.id}
-                                data={order}
-                            />
-                        );
-                    })
-                }
-            </div>
+                this.state.orders.length > 0 ? (
+                    <div>
+                    {
+                        this.state.orders.map(order => {
+                            return (
+                                <OrderItem
+                                    key={order.id}
+                                    data={order}
+                                />
+                            );
+                        })
+                    }
+                    </div>
+                ) : (
+                    <div></div>
+                )
         );
     }
 }
